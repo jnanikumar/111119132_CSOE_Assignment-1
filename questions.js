e4e2a14f48d73fa491740c1a61bf942504f43cac
jnani kumar <jnanik16@gmail.com>
2022-03-30 16:21:29 +0530

    csoe final submission

---
+++ questions.js
@@ -0,0 +1,28 @@
+// const quiz =[
+// {
+// 	q:'which month comes right before june',
+// 	options:['may','june','july','august'],
+// 	answer:0
+// }
+
+// {q:'what is your name',
+// 	options:['jnani','rohi','venkata','sulo'],
+// 	answer:0
+// }
+
+// {
+// 	q:'7+10',
+// 	options:[14,17,13,24],
+// 	answer:1
+// }
+
+
+// {
+// 	q:'9+10=18',
+// 	options:['true','false'],
+// 	answer:0
+// }
+// ]
+
+obj={a:2,b:3}
+obj.a
